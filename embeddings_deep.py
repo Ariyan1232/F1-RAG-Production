0f3983ce0fcc80a0c975cbb79aa65d0bd402715f
Ariyan Nagratha <ariyan.nagratha@gmail.com>
2026-09-16 00:42:37 -0400

    Added batch embeddigs and similarity search with cosine similarity

--- embeddings_deep.py
+++ embeddings_deep.py
@@ -19,5 +19,47 @@ def basic_embeddings():
     print(f"Vector values: {single_embedding[:5]}")
     print(f"Vector norm: {np.linalg.norm(single_embedding):.4f}")
 
+def batch_embeddings():
+    text = [
+        "What is F1",
+        "Who won the 2023 Monaco Grand Prix?",
+        "What is the Drag Reduction System (DRS)?"
+    ]
+
+    batch_embeddings = embeddings.embed_documents(text)
+    for i, emb in enumerate(batch_embeddings):
+        print(f"Vector dimensions: {len(emb)}")
+        print(f"Vector values: {emb[:5]}")
+        print(f"Vector norm: {np.linalg.norm(emb):.4f}")
+
+def similarity_search():
+
+    # Example document embeddings
+    docs = [
+        "Max Verstappen won the 2023 Monaco Grand Prix.",
+        "Fernando Alonso finished second in the 2023 Monaco GP.",
+        "Esteban Ocon completed the podium in third place."
+    ]
+
+    query = "What was the result of the 2023 Monaco Grand Prix?"
+
+    # embed documents and query
+    doc_vector = embeddings.embed_documents(docs)
+    query_vector = embeddings.embed_query(query)
+
+    #compute cosine similarity
+    def cosine_similarity(vec_a, vec_b):
+        return np.dot(vec_a, vec_b) / (np.linalg.norm(vec_a) * np.linalg.norm(vec_b))
+
+    similarities = [cosine_similarity(query_vector, doc_vec) for doc_vec in doc_vector]
+
+    # rank documents by similarity
+    ranked_docs = sorted(zip(docs, similarities), key=lambda x: x[1], reverse=True)
+
+    print(f"Query: {query}\n")
+    print("Ranked documents by similarity:")
+    for doc, score in ranked_docs:
+        print(f" {score:.4f}; {doc}")
+
 if __name__ == "__main__":
-    basic_embeddings()
+    similarity_search()
